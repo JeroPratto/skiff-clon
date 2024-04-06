8b4ce8c6c283d049da2e481cba6175a6ba884d90
Jeronimo Pratto <jeropratto@yahoo.com.ar>
2024-04-06 14:50:07 -0300

    implement lazy loading on routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,28 @@ import './App.css'
 import { Home } from './Pages/Home'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
-import { Mail } from './Pages/Mail'
-import { Pages } from './Pages/Pages'
-import { Drive } from './Pages/Drive'
-import { useEffect } from 'react'
-import { SignIn } from './Pages/SignIn'
+import { lazy, useEffect, Suspense } from 'react'
 
 function App() {
+	const Mail = lazy(() => import('./Pages/Mail/Mail'))
+	const Pages = lazy(() => import('./Pages/Pages/Pages'))
+	const Drive = lazy(() => import('./Pages/Drive/Drive'))
+	const SignIn = lazy(() => import('./Pages/SignIn/SignIn'))
 	return (
-		<BrowserRouter>
-			<ScrollTop />
-			<Header />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/mail' element={<Mail />} />
-				<Route path='/pages' element={<Pages />} />
-				<Route path='/drive' element={<Drive />} />
-				<Route path='/signin' element={<SignIn />} />
-			</Routes>
-			<Footer />
-		</BrowserRouter>
+		<Suspense>
+			<BrowserRouter>
+				<ScrollTop />
+				<Header />
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/mail' element={<Mail />} />
+					<Route path='/pages' element={<Pages />} />
+					<Route path='/drive' element={<Drive />} />
+					<Route path='/signin' element={<SignIn />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</Suspense>
 	)
 }
 
